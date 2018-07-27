409e418718b424bcee30533b273090b497056510
Rob <robert.erez@gmail.com>
2018-07-27 09:45:02 +0800

    Write message

--- Octopus_Startup.ps1
+++ Octopus_Startup.ps1
@@ -7,5 +7,5 @@ if($Environment -eq "") {
 	mv .\$Environment.env .\.env
 }
 $ProjectName="MyApp-$Environment"
-
+Write-Host Starting Project $ProjectName
 docker-compose --project-name $ProjectName up --detach --force-recreate 
