a97135987ebdcf7df7f01ec32869ae2fbe8da6f6
Rob <robert.erez@gmail.com>
2018-07-27 09:17:33 +0800

    Blah

---
+++ Octopus_Startup.ps1
@@ -0,0 +1,11 @@
+$Environment="#{Octopus.Environment.Name}"
+
+if($Environment -eq "#{Octopus.Environment.Name}") {
+	$Environment="local"
+} else {
+	mv .\.env .\.env.old
+	mv .\$Environment.env .\.env
+}
+$ProjectName="MyApp-$Environment"
+
+docker-compose --project-name $ProjectName up --detach 
